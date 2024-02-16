e0eba8abbfdcd1c95acf8efbe8eb78147e1df212
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-02-16 20:28:28 -0300

    feat: data structure definition

--- src/index.js
+++ src/index.js
@@ -10,7 +10,50 @@ request(url, function(err, response, html) {
         $('.tabela-expandir')
         .find('tr')
         .each(function(i) {
-            console.log(i+1);
+            const escudo = $(this)
+            .find('td')
+            .eq(0)
+            .find('img')
+            .attr('src');
+            const position = $(this)
+            .find('td')
+            .eq(0)
+            .find('b')
+            .text()
+            .trim();
+            const club = $(this)
+            .find('td')
+            .eq(0)
+            .find('span')
+            .eq(1)
+            .text()
+            .trim();
+            const points = $(this)
+            .find('td')
+            .eq(0)
+            .text()
+            .trim();
+            const games = $(this)
+            .find('td')
+            .eq(1)
+            .text()
+            .trim();
+            const wins = $(this)
+            .find('td')
+            .eq(2)
+            .text()
+            .trim();
+            const draws = $(this)
+            .find('td')
+            .eq(3)
+            .text()
+            .trim();
+            const loses = $(this)
+            .find('td')
+            .eq(4)
+            .text()
+            .trim();
+        console.log(escudo);
         })
     } else {
         console.log("Houve um problema ao abrir a url solicitada");
